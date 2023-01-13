e360f0b85d7a464b9660ef381499671cfd83aba1
Shree716 <122548030+Shree716@users.noreply.github.com>
2023-01-13 11:08:05 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Ghost)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Yes)
+})
+basic.showIcon(IconNames.Asleep)
